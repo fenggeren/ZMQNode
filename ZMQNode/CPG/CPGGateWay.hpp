2230aa361ec720d9b63e15c8c9d7745a0576f36d
fenggeren <huanao@huanaodemacbook-pro.local>
2018-07-31 18:04:05 +0800

    a little bit

--- ZMQNode/CPG/CPGGateWay.hpp
+++ ZMQNode/CPG/CPGGateWay.hpp
@@ -11,6 +11,7 @@
 #include <czmq.h>
 #include "Packet.h"
 #include "ZMQReactor.hpp"
+#include "server.pb.h"
 
 class CPGGateWay
 {
@@ -30,6 +31,84 @@ public:
             
         });
         reactor_.asyncLoop();
+    }
+    
+    void sendRegisterService()
+    {
+        CPG::ServiceRegisterRQ rq;
+        auto service = rq.mutable_service();
+        service->set_addr("127.0.0.1:2279");
+        service->set_servicetype(kGateWay);
+        
+        PacketHead head;
+        head.info = {0, 0, static_cast<unsigned int>(rq.ByteSize())};
+        head.command = {kGateWay, kSeviceRegisterRQ};
+        
+        google::protobuf::uint8* szBuf =
+        new google::protobuf::uint8[rq.ByteSize()];
+        rq.SerializeWithCachedSizesToArray(szBuf);
+        
+        zmsg_t* msg = zmsg_new();
+        
+        zmsg_addmem(msg, &head, sizeof(head));
+        zmsg_addmem(msg, szBuf, head.info.packetSize);
+        //
+        
+        //        zmsg_destroy(&msg);
+        
+        delete []szBuf;
+    }
+    
+    void clientMessageRead(zsock_t* sock)
+    {
+        zmsg_t* msg = zmsg_recv(sock);
+        size_t count = zmsg_size(msg);
+        printf("client receive begin\n");
+        
+        for (int i = 0; i < count - 1; i+=2)
+        {
+            zframe_t* headFrame = zmsg_next(msg);
+            PacketHead head;
+            memcpy(&head, zframe_data(headFrame), zframe_size(headFrame));
+            
+            
+            zframe_t* dataFrame = zmsg_next(msg);
+            char* data = reinterpret_cast<char*>(zframe_data(dataFrame));
+            size_t size = zframe_size(dataFrame);
+            parseClientData(head, data, size);
+        }
+        printf("\nreceive end\n");
+    }
+    
+    void parseClientData(const PacketHead& head,
+                         char* data, size_t len)
+    {
+        auto& command = head.command;
+        switch (command.mainCmdID) {
+            case kMaster:
+            {
+                switch (command.subCmdID) {
+                    case kSeviceRegisterRS:
+                        registerServiceCallback(head, data, len);
+                        break;
+                        
+                    default:
+                        break;
+                }
+            }
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    void registerServiceCallback(const PacketHead& head, char* data, size_t len)
+    {
+        CPG::ServiceRegisterRS rs;
+        rs.ParseFromArray(data, len);
+        
+        rs.PrintDebugString();
     }
     
 private:
--- ZMQNode/CPG/CPGMaster.hpp
+++ ZMQNode/CPG/CPGMaster.hpp
@@ -15,7 +15,9 @@
 #include <thread>
 #include <memory>
 #include <functional>
+#include <list>
 #include "ZMQReactor.hpp"
+#include "server.pb.h"
 
 class CPGMaster
 {
@@ -44,6 +46,106 @@ private:
     void messageIn(zsock_t* sock)
     {
         
+    }
+    
+    // gateWay需要连接的 services
+    void sendGateWayConnectors(const std::string& source)
+    {
+        CPG::ServiceRegisterRS rs;
+        
+        auto services = requiredConnectService(kGateWay);
+        for (auto& service : services)
+        {
+            auto profile = rs.add_connectservices();
+            profile->set_servicetype(service.serviceType);
+            profile->set_addr(service.addr);
+        }
+        
+        requiredConnectService(kGateWay);
+        
+        PacketHead head;
+        head.info = {0, 0, static_cast<unsigned int>(rs.ByteSize())};
+        head.command = {kMaster, kSeviceRegisterRS};
+        
+        google::protobuf::uint8* szBuf = new google::protobuf::uint8[rs.ByteSize()];
+        rs.SerializeWithCachedSizesToArray(szBuf);
+        
+        zmsg_t* msg = zmsg_new();
+        zmsg_addmem(msg, source.data(), source.size());
+        zmsg_addmem(msg, &head, sizeof(head));
+        zmsg_addmem(msg, szBuf, head.info.packetSize);
+        
+        // send
+        
+        //        zmsg_destroy(&msg);
+        delete []szBuf;
+    }
+    
+    // DEALER => ROUTER
+    void serverMessageRead(zmsg_t* msg)
+    {
+        size_t count = zmsg_size(msg);
+        zframe_t* source = zmsg_first(msg);
+        printf("server receive begin\n");
+        
+        const std::string sourceStr(reinterpret_cast<char*>(zframe_data(source)),
+                                    zframe_size(source));
+        
+        
+        for (int i = 0; i < count - 1; i+=2)
+        {
+            zframe_t* headFrame = zmsg_next(msg);
+            PacketHead head;
+            memcpy(&head, zframe_data(headFrame), zframe_size(headFrame));
+            
+            
+            zframe_t* dataFrame = zmsg_next(msg);
+            char* data = reinterpret_cast<char*>(zframe_data(dataFrame));
+            size_t size = zframe_size(dataFrame);
+            
+            parseData(sourceStr, head, data, size);
+        }
+        printf("\nreceive end\n");
+    }
+    
+    void parseData(const std::string& source,
+                   const PacketHead& head,
+                   char* data, size_t len)
+    {
+        PacketCommand command = head.command;
+        switch (command.subCmdID) {
+            case kSeviceRegisterRQ:
+                registerService(source, data, len);
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    void registerService(const std::string& source, const char* data, size_t len)
+    {
+        CPG::ServiceRegisterRQ rq;
+        rq.ParseFromArray(data, len);
+        
+        rq.PrintDebugString();
+        
+        auto& service = rq.service();
+        
+        ServiceProfile profile{service.servicetype(), ZMQ_DEALER,service.addr()};
+        // cal
+        
+        
+        sendGateWayConnectors(source);
+    }
+    
+    std::list<ServiceProfile> requiredConnectService(int type)
+    {
+        return {
+            {kLoginServer, ZMQ_ROUTER,"172.0.0.1:5558"},
+            {kMatchServer, ZMQ_ROUTER, "172.0.0.1:5558"},
+            {kMatchManager, ZMQ_ROUTER,"172.0.0.1:5558"}
+        };
     }
     
 private:
